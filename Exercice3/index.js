829564216b6d4e897657090d9e5c5329826ac7d2
raven-panda <ambroise.maire7@gmail.com>
2025-03-07 11:13:18 +0100

    add: impemented test exercice 3

--- Exercice3/index.js
+++ Exercice3/index.js
@@ -1,5 +1,13 @@
 // Créez votre fonction ici
 
+function calculateAverage(arr) {
+    if (!Array.isArray(arr) || arr.length < 1)
+        return 'No numbers to calculate average';
+
+    const additionTotal = arr.reduce((acc, cur) => (typeof acc === 'string' ? parseInt(acc) : acc) + cur);
+    return additionTotal / arr.length;
+}
+
 // Exemples d'utilisation de la fonction
 console.log(calculateAverage([5, 10, 15])) // retourne 10
 console.log(calculateAverage([10, 20, 30, 20])) // retourne 20
