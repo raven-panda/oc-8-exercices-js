3f2c94763ce6d2a1f13459844b92042048d962db
raven-panda <ambroise.maire7@gmail.com>
2025-03-07 11:01:59 +0100

    add: impemented test exercice 1

--- Exercice1/index.js
+++ Exercice1/index.js
@@ -1,3 +1,15 @@
 //votre code ici
 
-export default pairNumbers
+function pairNumbers(min, max) {
+    const parsedMin = typeof min === 'string' ? parseInt(min) : min,
+        parsedMax = typeof max === 'string' ? parseInt(max) : max;
+
+    let final = [];
+    for (let i = parsedMin; i <= parsedMax; i++ ) {
+        if (i % 2 === 0) final.push(i);
+    }
+
+    return final.join(',');
+}
+
+export default pairNumbers;
